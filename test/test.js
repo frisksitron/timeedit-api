84d74b67f441bb4fa94562b8dfe8374dc71d9127
Nataniel Pedersen <natanielmp@gmail.com>
2016-07-01 15:40:09 +0200

    Basic tests

---
+++ test/test.js
@@ -0,0 +1,33 @@
+const expect = require('chai').expect;
+const TimeEdit = require('../timeedit.js');
+
+let time = new TimeEdit(
+  'https://no.timeedit.net/web/hib/db1/aistudent/'
+);
+
+describe('TimeEdit api', () => {
+  it('should return schedule id', () => {
+    time.searchSchedule()
+      .then((result) => {
+        expect(result).to.be.a('number');
+      }).catch((err) => {
+        console.log(err);
+      });
+  });
+  it('should return types as object', () => {
+    time.getTypes()
+      .then((result) => {
+        expect(result).to.be.a('object');
+      }).catch((err) => {
+        console.log(err);
+      });
+  });
+  it('should return schedule as object', () => {
+    time.getSchedule()
+      .then((result) => {
+        expect(result).to.have.property('reservations');
+      }).catch((err) => {
+        console.log(err);
+      });
+  });
+});
